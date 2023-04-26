7682015e08721dd363ff8cf5ed5586be60eb302c
Dikra Bouabdellah <dikra.bouabdellah@student.ehb.be>
2023-04-26 10:08:41 +0200

    inserting the map

--- js/index.js
+++ js/index.js
@@ -3,27 +3,24 @@
 // de leaflet library is reeds geimporteerd, en beschikbaar als "L"
 // dit via de script en css tag in de index.html, en de "map" div die werd toegevoegd.
 
-
 const app = {
-    map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
-    init() {
-        // initialise de kaart
+	map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
+	init() {
+		var map = L.map("map").setView([51.505, -0.09], 13);
 
-        // voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-        // vergeet openstreetmap attributie niet
-        
-        // gebruik de functie "loadMarkers" om de markers toe te voegen
-    },
-    loadMarkers() {
+		L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		}).addTo(map);
 
-        // fetch de data van opendata.brussels.be
-            // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
-        
-        
-    },
-    addMarker(lat, lon) {
-        // voeg een marker toe op lat, lon
-    }
-}
+		L.marker([51.5, -0.09]).addTo(map).bindPopup("A pretty CSS3 popup.<br> Easily customizable.").openPopup();
+	},
+	loadMarkers() {
+		// fetch de data van opendata.brussels.be
+		// als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
+	},
+	addMarker(lat, lon) {
+		// voeg een marker toe op lat, lon
+	},
+};
 
 app.init();
